d9c944488a841136b6d283b4ee59391f05f41585
Krista Blachian <KristaBlachian@outlook.com>
2018-04-18 20:31:35 -0400

    Project complete. IntelliJ uncooperative with checkbox grid.

--- src/com/theironyard/BeatBox.java
+++ src/com/theironyard/BeatBox.java
@@ -3,8 +3,8 @@ package com.theironyard;
 import java.awt.*;
 import javax.swing.*;
 import javax.sound.midi.*;
-import java.util.*;
 import java.awt.event.*;
+import java.util.*;
 
 public class BeatBox {
 
@@ -27,6 +27,7 @@ public class BeatBox {
         new BeatBox().buildGUI();
     }
 
+
     public void buildGUI() {
         theFrame = new JFrame("Cyber BeatBox");
         theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -37,7 +38,7 @@ public class BeatBox {
         //an 'empty border' gives us a margin between the edges of the panel and
         //where the components are placed. Purely aesthetic.
 
-        checkboxList = new ArrayList<JCheckBox>();
+        checkboxList = new ArrayList<>();
         Box buttonBox = new Box(BoxLayout.Y_AXIS);
 
         JButton start = new JButton("Start");
@@ -57,8 +58,10 @@ public class BeatBox {
         buttonBox.add(downTempo);
 
         Box nameBox = new Box(BoxLayout.Y_AXIS);
-        for (int i = 0; i <16; i++) {
+        for (int i = 0; i < 16; i++) {
             nameBox.add(new Label(instrumentNames[i]));
+            //i < 16; threw ArrayIndexOutOfBoundsException.
+            //changing to i < instrumentNames.length fixed it.
         }
 
         background.add(BorderLayout.EAST, buttonBox);
@@ -72,7 +75,7 @@ public class BeatBox {
         mainPanel = new JPanel(grid);
         background.add(BorderLayout.CENTER, mainPanel);
 
-        for (int i= 0; i< 256; i++); {
+        for (int i = 0; i < 256; i++); {
             JCheckBox c = new JCheckBox();
             c.setSelected(false);
             checkboxList.add(c);
@@ -80,6 +83,7 @@ public class BeatBox {
             //Make the checkboxes, set them to 'false' (so they aren't checked)
             //and add them to the ArrayList AND to the GUI panel.
         }//end loop
+
         setUpMidi();
 
         theFrame.setBounds(50,50,300,300);
@@ -95,6 +99,122 @@ public class BeatBox {
             track = sequence.createTrack();
             sequencer.setTempoInBPM(120);
 
-        }catch(Exception e) {e.printStackTrace();}
-    }//close method
-}
+        } catch (Exception e) {e.printStackTrace();}
+    }//close MIDI method
+
+    public void buildTrackAndStart() {
+        int[] trackList = null;
+        //make a 16-element array to hold the values for one instrument,
+        //across all 16 beats. If the instrument is supposed to play on
+        // that beat, the value at that element will be the key. If that
+        // instrument is NOT supposed to play on that beat, put in a zero.
+
+        sequence.deleteTrack(track);
+        track = sequence.createTrack();
+        //get rid of the old track, make a fresh one.
+
+        for (int i = 0; i < 16; i++) {
+            trackList = new int[16];
+            //do this for each of the 16 rows(i.e. Bas, Conga, etc.)
+
+            int key = instruments[i];
+            //Set the 'key' that represents which instrument this is (Bass,
+            //Hi-Hat, etc. The instruments array holds the actual MIDI numbers
+            // for each instrument.)
+
+            for (int j = 0; j < 16; j++) {
+
+                JCheckBox jc = checkboxList.get(j + 16*i);
+                if (jc.isSelected()) {
+                    trackList[j] = key;
+                } else {
+                    trackList[j] = 0;
+                } //if the checkbox at this beat is selected, put the key
+                //value in this slot in the array. (the slot that represents
+                //the beat.) Otherwise, the instrument is NOT supposed to play
+                //at this beat, so set it to zero.
+            }//close inner loop
+
+            makeTracks(trackList);
+            track.add(makeEvent(176, 1, 127, 0, 16));
+            //For this instrument, and for all 16 beats,
+            // make events and add them to the track.
+        }//close outer
+
+        track.add(makeEvent(192, 9, 1, 0, 15));
+        //we always want to make sure that there IS an event at beat 16(it goes
+        //0 to 15) Otherwise, the BeatBox might not go the full 16 beats before
+        // it starts over.
+        try {
+
+            sequencer.setSequence(sequence);
+            sequencer.setLoopCount(sequencer.LOOP_CONTINUOUSLY);
+            //Lets you specify the number of loop iterations, or in this case
+            //continuous looping
+            sequencer.start();
+            sequencer.setTempoInBPM(120);
+        } catch (Exception e) { e.printStackTrace(); }
+        //now play it!
+    }//close buildTrackAndStart method
+
+        public class MyStartListener implements ActionListener {
+            public void actionPerformed(ActionEvent a) {
+                buildTrackAndStart();
+            }
+        }//close inner class
+
+        public class MyStopListener implements ActionListener {
+            public void actionPerformed(ActionEvent a) {
+             sequencer.stop();
+            }
+        }//close inner class
+
+        public class MyUpTempoListener implements ActionListener {
+            public void actionPerformed(ActionEvent a) {
+                float tempoFactor = sequencer.getTempoFactor();
+                sequencer.setTempoFactor((float)(tempoFactor * 1.03));
+                //The Tempo Factor scales teh sequencer's tempo by the factor
+                //provided. The default is I.O, so we're adjusting +/-3%
+                //per click.
+            }
+        }//close inner class
+
+        public class MyDownTempoListener implements ActionListener {
+            public void actionPerformed(ActionEvent a) {
+                float tempoFactor = sequencer.getTempoFactor();
+                sequencer.setTempoFactor((float)(tempoFactor * .97));
+            }
+        }
+        public void makeTracks(int[] list) {
+            //Makes events for one instrument at a teime, for all 16 beats. So it
+        //might get an int[] for the Bass drum, and each index in the array will
+        //hold either the key of that instrument, or a zero. If it's a zero, the
+        //instrument isn't supposed to play at that beat. Otherwise, make an event
+        //and add it to the track.
+
+            for (int i = 0; i < 16; i++) {
+                int key = list[i];
+                if (key !=0) {
+                    track.add(makeEvent(144,9,key,100,i));
+                    track.add(makeEvent(128,9,key,100,i+1));
+                    //make the NOTE ON and NOTE OFF events, and add
+                    // them to the track.
+                }
+            }
+    }//close makeTracks method
+
+            public MidiEvent makeEvent(int comd, int chan, int one,
+                  int two, int tick) {
+            MidiEvent event = null;
+            try {
+                ShortMessage a = new ShortMessage();
+                a.setMessage(comd, chan, one, two);
+                event = new MidiEvent(a, tick);
+
+            }catch(Exception e) {e.printStackTrace();}
+            return event;
+            }
+    }//close class
+
+
+
